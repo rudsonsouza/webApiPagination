8f0c200e0420ea20aa008ad0069f0a05a8b0f54b
Rudson Souza <rudson-souza@hotmail.com>
2023-08-19 14:06:17 -0300

    Web database pagination

---
+++ webPagination/Controllers/TodoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webPagination.Data;
+using webPagination.Models;
+
+namespace webPagination.Controllers
+{
+	[ApiController]
+	[Route("v1/todos")]
+	public class TodoController : ControllerBase
+	{
+		[HttpGet("load")]
+		public async Task<IActionResult> LoadAsync(
+			[FromServices] AppDbContext context)
+		{
+			for (int i = 0; i < 1348; i++)
+			{
+				var todo = new Todo()
+				{
+					Id = i + 1,
+					Done = false,
+					CreatedAt = DateTime.Now,
+					Title = $"Tarefa {i}"
+				};
+				await context.Todos.AddAsync(todo);
+				await context.SaveChangesAsync();
+			}
+
+			return Ok();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAsync(
+			[FromServices]AppDbContext context,
+			[FromQuery] int skip = 0,
+			[FromQuery] int take = 25)
+		{
+			var todos = await context.Todos
+				.AsNoTracking()
+				.Skip(skip)
+				.Take(take)
+				.ToListAsync();		
+
+			return Ok(todos);
+		}
+	}
+}
+
---
+++ webPagination/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using webPagination.Models;
+
+namespace webPagination.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<Todo> Todos { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+    }
+}
+
